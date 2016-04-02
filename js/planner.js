45869b675b5145092475446014444b89e2563f3b
nadjathan <nadjanaomi@mac.com>
2016-04-02 17:07:03 +0000

    Done.

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,13 @@
 // Code Your Solution Here
+console.log("Test");
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var trip_destination = prompt("What is your trip destination?");
+var number_of_travelers = prompt("How many people are you traveling with?");
+var type_of_trip = prompt("What are you looking for in trip?");
+
+alert(name + " who is " + age + " years old is going to " + trip_destination + ". They are traveling with " + number_of_travelers + ".They are going to have a " + type_of_trip + " trip." );
+
+if(type_of_trip == "relaxing" || type_of_trip == "Relaxing"){
+  alert("You should go on a beach vacation");
+}
